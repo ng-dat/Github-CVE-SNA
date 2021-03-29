66e4f624d5bfc144f4f5a0099aed02c9a5fba1c4
Nguyen Cong Dat <traceformula2000@gmail.com>
2021-03-29 13:00:16 +0700

    v1.0.3 | Visualization & Scaling-up | Setup server config

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
+import private_config
+
+# neo4j = dict(
+#     endpoint = "bolt://localhost:7687",
+#     db_name = "git2neo",
+#     user_name = "dat1",
+#     password = "1",
+# )
+
 neo4j = dict(
     endpoint = "bolt://localhost:7687",
-    db_name = "git2neo",
-    user_name = "dat1",
-    password = "1",
+    db_name = "neo4j",
+    user_name = "neo4j",
+    password = private_config.server_neo4j_pass,
 )
+
+
