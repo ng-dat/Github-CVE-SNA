3f8317d2e441cbf77790ad5ebe216140642e46de
Nguyen Cong Dat <traceformula2000@gmail.com>
2021-03-29 12:12:00 +0700

    v1.0.3 | Visualization & Scaling-up | Get "follow" relationship between users

--- utils/neo_queries.py
+++ utils/neo_queries.py
@@ -1,6 +1,21 @@
 from py2neo import Graph, Node, Relationship
 
 
+def query_all_usernames(neo4j_graph):
+    """
+    Get users who starred the most number of repositories from Neo4j
+    :param neo4j_graph: <type py2neo.Graph>, represents Neo4j database connection
+    :param limit_users: <type number>, number of users to query
+    :return:
+    """
+    cypher_query = '''
+        MATCH (n:Person) 
+        RETURN n.username as name  
+    '''
+    user_nodes = neo4j_graph.run(cypher_query).data()
+    usernames = [str(x['name']) for x in user_nodes]
+    return usernames
+
 def query_most_cve_starred_users(neo4j_graph, limit_users):
     """
     Get users who starred the most number of repositories from Neo4j
